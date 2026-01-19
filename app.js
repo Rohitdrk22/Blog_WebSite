9bd4086e88e288e5ad502b101999a05e5af904ec
Rohit <rohitdrk22@gmail.com>
2026-01-19 22:26:54 +0530

    Second commit of website of app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import methodOverride from "method-override";
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const DATA_FILE = path.join(__dirname, "data.json");
